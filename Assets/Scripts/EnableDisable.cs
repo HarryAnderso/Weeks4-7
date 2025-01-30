94a95d2bfa544351cc0e2d4abdce27836b980ec1
Harry Anderson <andeharr@shernet.sheridancollege.ca>
2025-01-30 10:49:18 -0500

    here

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -8,6 +8,8 @@ public class EnableDisable : MonoBehaviour
     public SpriteRenderer sr;
     public EnableDisable script;
     public GameObject go;
+    public AudioSource aus;
+    public AudioClip clip;
     void Start()
     {
         
@@ -21,12 +23,15 @@ public class EnableDisable : MonoBehaviour
             sr.enabled=false;
             //script.enabled = false;
             go.SetActive(false);
+
         }
         if(Input.GetKeyDown(KeyCode.Space))
         {
             sr.enabled = true;
             //script.enabled = true;
             go.SetActive(true);
+            aus.Play();
+            aus.PlayOneShot(clip);
         }
         
     }
