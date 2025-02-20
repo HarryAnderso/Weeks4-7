9bcf93f40c1908f9bfd683bed54430088d865f37
harryanderson <harrystuartanderson@gmail.com>
2025-02-19 19:50:15 -0500

    No clue whats wrong

---
+++ Assets/Food Sprites/NewMonoBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+    void Start()
+    {
+
+    }
+
+    // Update is  once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefab);
+        }
+    }
+}
