b6bdcd4b94543cc0aafc0e031d9ac1a2a54f2732
harryanderson <harrystuartanderson@gmail.com>
2025-02-19 19:37:40 -0500

    testing if changes are recognized

--- Assets/Music/Cloudscript.cs
+++ Assets/Music/Cloudscript.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class Cloudscript : MonoBehaviour
 {
     public float cspeed = 5;
-    void Start()
+    public float life = 1;
+    public void Start()
     {
-        
+        Destroy(gameObject, 5);
+        Vector3 pos = transform.position;
+
+
+        //cspeed = Random.Range(-5, 5);
+        pos.y = Random.Range(0, 4);
+        transform.position = pos;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called  per frame
+    public void Update()
     {
+        
         Vector3 pos = transform.position;
         pos.x += cspeed * Time.deltaTime;
-        transform.position = pos;
+        
         Vector2 screenpos = Camera.main.WorldToScreenPoint(pos);
 
         if(screenpos.x < 0 || screenpos.x >Screen.width)
@@ -23,5 +31,17 @@ public class Cloudscript : MonoBehaviour
             cspeed = cspeed * -1;
         }
 
+        life -= Time.deltaTime;
+
+        Vector3 sc = transform.localScale;
+        sc.x = life/5;
+        sc.y = life/5;
+        transform.localScale = sc;
+        pos.y +=life/50000;
+        transform.position = pos;
+    }
+    public void speedset(float speedf)
+    {
+        cspeed = speedf;
     }
 }
